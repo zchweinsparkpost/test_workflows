72c9938c0601a0e1d92378ef90c5240f5c4479b3
Zach S <zach.schweinfurth@sparkpost.com>
2019-11-25 14:19:49 -0500

    changes

--- do_thing.py
+++ do_thing.py
@@ -1,7 +1,12 @@
 import subprocess
 
-output = subprocess.run(["git", "diff", "--name-status", "refs/heads/master -- /refs/heads/stg"], stdout=subprocess.PIPE)
-output_data = output.stdout.decode("utf-8")
-output_data_lines = output_data.splitlines()
-out_lines_transform = map(lambda val: val.split("\t"), output_data_lines)
-print(list(out_lines_transform))
+
+def get_git_status(from_branch, to_branch):
+    output = subprocess.run(["git", "diff", "--name-status", "{}..{}".format(from_branch, to_branch)], stdout=subprocess.PIPE)
+    output_data = output.stdout.decode("utf-8")
+    output_data_lines = output_data.splitlines()
+    out_lines_transform = map(lambda val: val.split("\t"), output_data_lines)
+    print(list(out_lines_transform))
+
+
+get_git_status("master", "remotes/origin/stg")
