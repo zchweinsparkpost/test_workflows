c59ffb7a900ac92cfa50a32eccd730c00e6f23a6
Zach S <zach.schweinfurth@sparkpost.com>
2019-11-25 12:42:07 -0500

    changes

--- do_thing.py
+++ do_thing.py
@@ -1,6 +1,6 @@
 import subprocess
 
-output = subprocess.run(["git", "diff", "--name-status", "master..stg"], capture_output=True)
+output = subprocess.run(["git", "diff", "--name-status", "master..stg"], stdout=subprocess.PIPE)
 output_data = output.stdout.decode("utf-8")
 output_data_lines = output_data.splitlines()
 out_lines_transform = map(lambda val: val.split("\t"), output_data_lines)
