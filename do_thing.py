7e1f884c73c1b081f071b747a50c2ad02d1423ab
Zach S <zach.schweinfurth@sparkpost.com>
2019-11-25 12:39:28 -0500

    doing a thing

--- do_thing.py
+++ do_thing.py
@@ -4,4 +4,4 @@ output = subprocess.run(["git", "diff", "--name-status", "master..stg"], capture
 output_data = output.stdout.decode("utf-8")
 output_data_lines = output_data.splitlines()
 out_lines_transform = map(lambda val: val.split("\t"), output_data_lines)
-print(list(out_lines_transform))`
+print(list(out_lines_transform))
